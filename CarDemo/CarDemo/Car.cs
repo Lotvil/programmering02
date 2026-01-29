e1284e304b389ade7edb5011828571e3fc23362a
Lotvil <b.mcsinen@gmail.com>
2026-01-29 10:23:22 +0100

    u01 klar

---
+++ CarDemo/CarDemo/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarDemo
+{
+    public class Car
+    {
+        //
+        // Medlemsvariabler
+        //
+        public String regNr;    //registreringsnummer
+        public String make;     //bilmärke
+        public String model;    //bilmodell
+        public int year;        //årsmodell
+        public bool forSale;    //är bilen till salu?
+
+        //
+        // Defaultkonstruktor
+        //
+        public Car()
+        {
+        }
+    }
+}
--- moment01/u01/Program.cs
+++ moment01/u01/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! hej");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Jag heter Leonid och jag läser programmering på Alströmergymnasiet.");
+            Console.WriteLine("Jag tycker att det verkar \"kul\" med programmering.");
+            Console.WriteLine("Dessa tre rader\nhar jag skrivit med\nen enda intruktion.");
+            Console.WriteLine("Följande saker har jag kommenterat i min kod:");
+            Console.WriteLine("\t* Hur jag skrev ut dubbelfnuttarna på andra raden.\n\t* Hur jag kunde skriva ut tre rader med en enda intruktion.");
+
         }
     }
 }
