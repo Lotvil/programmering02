62a4eaa8c0a814e64195e5e8dd138c1a4c85dc23
Lotvil <b.mcsinen@gmail.com>
2026-01-29 16:12:56 +0100

    u03-05 klar

--- moment01/u03/u03/Program.cs
+++ moment01/u03/u03/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Clear();
+
+            Console.WriteLine("Hur många pennor vill du köpa?");
+            int pennor = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vad kostar pennorna styck?");
+            double kostnadPenna = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hur mycket äpplen vill du köpa i vikt(kg)?");
+            double äpple = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hur mycket kostar äpplen/kg?");
+            double äppleKostnad = Convert.ToDouble(Console.ReadLine());
+
+            double kostnadPennor = pennor * kostnadPenna;
+            double kostnadÄpplen = äpple * äppleKostnad;
+            double totalKostnad = kostnadPennor + kostnadÄpplen;
+
+            Console.WriteLine($"Jag handlade {pennor} pennor för {kostnadPennor} och 1 äpple för {kostnadÄpplen:#.00} vilket blev totalt {totalKostnad:#.00}kr");
         }
     }
 }
---
+++ u04/u04/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace u04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Clear();
+
+            DateTime dt = DateTime.Now;
+            int hour = dt.Hour;
+
+            string u;
+
+            if(hour >= 8)
+            {
+                u = "Skoldagen pågår";
+            }
+            else if (hour > 17)
+            {
+                u = "Skoldagen har slutat";
+            }
+            else
+            {
+                u = "Skoldagen har inte börjat";
+            }
+            Console.WriteLine(u);
+        }
+    }
+}
