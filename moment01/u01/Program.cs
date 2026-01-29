cef4e6ac3f49644d563712e624fb04112334fcb5
Lotvil <b.mcsinen@gmail.com>
2026-01-29 10:43:58 +0100

    changed names and file path

--- moment01/u01/Program.cs
+++ moment01/u01/Program.cs
@@ -1,16 +1,8 @@
-﻿namespace u01
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Jag heter Leonid och jag läser programmering på Alströmergymnasiet.");
-            Console.WriteLine("Jag tycker att det verkar \"kul\" med programmering.");
-            Console.WriteLine("Dessa tre rader\nhar jag skrivit med\nen enda intruktion.");
-            Console.WriteLine("Följande saker har jag kommenterat i min kod:");
-            Console.WriteLine("\t* Hur jag skrev ut dubbelfnuttarna på andra raden.\n\t* Hur jag kunde skriva ut tre rader med en enda intruktion.");
+﻿using System;
 
-        }
-    }
+public class Class1
+{
+	public Class1()
+	{
+	}
 }
