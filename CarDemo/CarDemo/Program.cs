0f594a2e1a6415c1213d66f1a24e6bb231a9c22a
Lotvil <b.mcsinen@gmail.com>
2026-01-29 09:46:53 +0100

    created CerDemo

---
+++ CarDemo/CarDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CarDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ moment01/u01/u02.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace u01
+{
+    internal class u02
+    {
+    }
+}
