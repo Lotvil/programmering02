570f3eb7f70033e664c09c0162e2a39ffe20c2c4
Lotvil <b.mcsinen@gmail.com>
2026-01-29 10:36:57 +0100

    u02 klar

--- u02/u02/Program.cs
+++ u02/u02/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Clear();
+
+            int pennor = 3;
+            int kostnadPenna = 4;
+            double äpple = 0.2;
+            int äppleKostnad = 19;
+
+            double kostnadPennor = pennor * kostnadPenna;
+            double kostnadÄpplen = äpple * äppleKostnad;
+            double totalKostnad = kostnadPennor + kostnadÄpplen;
+
+            Console.WriteLine($"Jag handlade {pennor} pennor för {kostnadPennor} och 1 äpple för {kostnadÄpplen:#.00} vilket blev totalt {totalKostnad:#.00}kr");
         }
     }
 }
