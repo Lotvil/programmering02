74301a78890c82f27568fb801e75cb3d60bd05dd
Lotvil <b.mcsinen@gmail.com>
2026-01-29 20:59:35 +0100

    Car.Demo klar

--- CarDemo/CarDemo/Program.cs
+++ CarDemo/CarDemo/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Clear();
+
+            Console.WriteLine("Ange information om bilen");   // Skriver ut till konsollen
+
+            Console.Write("Registreringsnummer: ");          // Registreringsnummer
+            string regNr = Console.ReadLine();
+
+            Console.Write("Bilmärke: ");                      // Bilmärke
+            string make = Console.ReadLine();
+
+            Console.Write("Modell: ");                        // Modell
+            string model = Console.ReadLine();
+
+            Console.Write("Årsmodell: ");                     // Årsmodell
+            int year = Convert.ToInt16(Console.ReadLine());   // Konverteras till int
+
+            Console.Write("Till salu (J/N): ");               // Till salu?
+            char ch = Convert.ToChar(Console.Read());         // Konverteras till char
+
+            bool forSale = false;
+            if (Char.ToUpper(ch) == 'J')
+            {
+                forSale = true;
+            }
+
+            // Skapa ny bil med inmatade värden
+            Car c = new Car(regNr, make, model, year, forSale);
+
+            // Skriv ut info om bilen c
+            Console.WriteLine("\n" + c.ToString());
         }
     }
 }
---
+++ moment01/u09/u09/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+
+namespace u09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Clear();
+
+
+
+            Console.WriteLine("Hur många tärningsslag vill du göra?");
+            int kast = Convert.ToInt32(Console.ReadLine());
+
+            Random r = new Random();
+            List<int> tärningar = new List<int>();
+
+            for (int i = 0; i < kast; i++)
+            {
+                int tärning = r.Next(1, 7);
+                tärningar.Add(tärning);
+            }
+            for (int i = 1; i < 7; i++)
+            {
+                Console.WriteLine($"Du slog {tärningar.Count(t => t == i)} kast med sidan {i}");
+            }
+        }
+    }
+}
