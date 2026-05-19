fc15796498a3a9ae464a343f028ad11790f1ada9
Lotvil <b.mcsinen@gmail.com>
2026-05-19 13:49:08 +0200

    CarAPI Done

---
+++ CarAPI/CarAPI/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace CarAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new System.Windows.Forms.Label();
+            this.txbRegNr = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtCarInfo = new System.Windows.Forms.RichTextBox();
+            this.tlpCar = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(54, 36);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(47, 16);
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = "RegNr";
+            // 
+            // txbRegNr
+            // 
+            this.txbRegNr.Location = new System.Drawing.Point(138, 36);
+            this.txbRegNr.Name = "txbRegNr";
+            this.txbRegNr.Size = new System.Drawing.Size(100, 22);
+            this.txbRegNr.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(57, 75);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 50);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Sök";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtCarInfo
+            // 
+            this.txtCarInfo.Location = new System.Drawing.Point(459, 12);
+            this.txtCarInfo.Name = "txtCarInfo";
+            this.txtCarInfo.Size = new System.Drawing.Size(329, 426);
+            this.txtCarInfo.TabIndex = 3;
+            this.txtCarInfo.Text = "";
+            // 
+            // tlpCar
+            // 
+            this.tlpCar.ColumnCount = 2;
+            this.tlpCar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpCar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpCar.Location = new System.Drawing.Point(57, 166);
+            this.tlpCar.Name = "tlpCar";
+            this.tlpCar.RowCount = 6;
+            this.tlpCar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlpCar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlpCar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlpCar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlpCar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlpCar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlpCar.Size = new System.Drawing.Size(247, 250);
+            this.tlpCar.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tlpCar);
+            this.Controls.Add(this.txtCarInfo);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txbRegNr);
+            this.Controls.Add(this.lbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.TextBox txbRegNr;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.RichTextBox txtCarInfo;
+        private System.Windows.Forms.TableLayoutPanel tlpCar;
+    }
+}
+
---
+++ CarAPI/CarAPI/Form1.cs
@@ -0,0 +1,174 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace CarAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // Skapar labels som skall läggas i tabellen
+            Label l1 = new Label();
+            l1.Text = "RegNr";
+            Label l2 = new Label();
+            l2.Text = "Typ";
+            Label l3 = new Label();
+            l3.Text = "Märke";
+            Label l4 = new Label();
+            l4.Text = "Modell";
+            Label l5 = new Label();
+            l5.Text = "Årsmodell";
+            Label l6 = new Label();
+            l6.Text = "Färg";
+
+            // Fetmarkera
+            l1.Font = new Font(l1.Font, FontStyle.Bold);
+            l2.Font = new Font(l2.Font, FontStyle.Bold);
+            l3.Font = new Font(l3.Font, FontStyle.Bold);
+            l4.Font = new Font(l4.Font, FontStyle.Bold);
+            l5.Font = new Font(l5.Font, FontStyle.Bold);
+            l6.Font = new Font(l6.Font, FontStyle.Bold);
+
+            // Lägg in labels i tabellen
+            int row = 0;
+            tlpCar.Controls.Add(l1, 0, row++);
+            tlpCar.Controls.Add(l2, 0, row++);
+            tlpCar.Controls.Add(l3, 0, row++);
+            tlpCar.Controls.Add(l4, 0, row++);
+            tlpCar.Controls.Add(l5, 0, row++);
+            tlpCar.Controls.Add(l6, 0, row++);
+
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // Om regNr är inmatat
+            if (txbRegNr.Text.Length > 0)
+            {
+                // Hämta regNr
+                string regNr = txbRegNr.Text.ToUpper();
+
+                // Nollställ textboxen
+                txbRegNr.Text = "";
+
+                // Anropa metoden printData
+                printData(regNr);
+            }
+            else
+            {
+                MessageBox.Show("Du måste ange ett registreringsnummer.", "Felaktig inmatning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void printData(string regNr)
+        {
+            // Variabler
+            string token = "jtBt2fWF8vS0v1B5EcZT9111j9YExcUfiLHq5-4xSWY";
+            string call = $"https://data.biluppgifter.se/api/v1/lookup/vehicle/regno/{regNr}";
+
+            try
+            {
+                // Skapar ett objekt med API-anropet till HttpWebRequest
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(call);
+                request.Method = "GET";
+                request.Headers["Authorization"] = $"Bearer {token}";
+
+                // Skapar ett svarsobjekt
+                WebResponse response = request.GetResponse();
+
+                // Läser av strömmen från svaret
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                // Läser in innehållet till en JSON-sträng
+                String car_JSON = reader.ReadToEnd();
+
+                
+                // Skapar ett JObject.
+                JObject jo = JObject.Parse(car_JSON);
+
+                // Skriver ut hela objektet
+                //txtJSON.Text = jo.ToString();
+
+                // Hämtar data från JSON-objektet och formaterar det till en sträng, om den är tom så sätts den till en tom sträng
+                string type = jo.SelectToken("vehicle.type")?.ToString() ?? "";
+                string make = jo.SelectToken("vehicle.make")?.ToString() ?? "";
+                string model = jo.SelectToken("vehicle.model")?.ToString() ?? "";
+                string vehicleYear = jo.SelectToken("vehicle.vehicle_year")?.ToString() ?? "";
+                string color = jo.SelectToken("vehicle.color")?.ToString() ?? "";
+
+                // Justerad
+                String car = String.Format($"{"Registreringnr":20} {regNr:10}");
+                car += String.Format($"{"\nTyp":20} {type}");
+                car += String.Format($"{"\nMärke":20} {make}");
+                car += String.Format($"{"\nModell":20} {model}");
+                car += String.Format($"{"\nÅrsmodell":20} {vehicleYear}");
+                car += String.Format($"{"\nFärg":20} {color}");
+                
+                txtCarInfo.Text = car;
+                
+                // Koppla kolumn 2
+                Label l1 = new Label();
+                Label l2 = new Label();
+                Label l3 = new Label();
+                Label l4 = new Label();
+                Label l5 = new Label();
+                Label l6 = new Label();
+
+                l1.Text = regNr;
+                l2.Text = type;
+                l3.Text = make;
+                l4.Text = model;
+                l5.Text = vehicleYear;
+                l6.Text = color;
+
+                ClearColumn(1);
+
+                int row = 0;
+                tlpCar.Controls.Add(l1, 1, row++);
+                tlpCar.Controls.Add(l2, 1, row++);
+                tlpCar.Controls.Add(l3, 1, row++);
+                tlpCar.Controls.Add(l4, 1, row++);
+                tlpCar.Controls.Add(l5, 1, row++);
+                tlpCar.Controls.Add(l6, 1, row++);
+            }
+            catch (Exception e)
+            {
+                //Töm textrutan
+                txtCarInfo.Text = "";
+
+                //Skriv ut felmeddelande
+                MessageBox.Show($"Din sökning på registreringsnummer {regNr} gav ingen träff.\nMeddelande: {e.Message}", "Ingen träff i databasen", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void ClearColumn(int column)
+        {
+            for (int i = tlpCar.Controls.Count - 1; i >= 0; i--)
+            {
+                Control control = tlpCar.Controls[i];
+
+                if (tlpCar.GetColumn(control) == column)
+                {
+                    tlpCar.Controls.Remove(control);
+                    control.Dispose();
+                }
+            }
+        }
+    }
+}
